8f60b7ee90942a46a63da91342fbd97b032bcf3c
Sven Apel <svap1201@gmail.com>
2026-09-22 15:53:16 +0200

    Product list and header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import  {ProductList} from './shared/components/product-list/product-list';
+
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, ProductList],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
@@ -10,3 +13,4 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('product-list');
 }
+
