f33efad4484549b401bd91ddd62b7c36dd34db22
Sven Apel <svap1201@gmail.com>
2026-09-23 10:13:57 +0200

    Add product detail component with interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import  {ProductList} from './shared/components/product-list/product-list';
+import  {ProductDetail} from './shared/components/product-detail/product-detail';
 
 
 @Component({
-  imports: [RouterOutlet, Header, ProductList],
+  imports: [RouterOutlet, Header, ProductList, ProductDetail],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
